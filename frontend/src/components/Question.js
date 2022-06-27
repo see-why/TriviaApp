f51425c51a235d602cd6eadc04551409ac456b70
see-why <iyadicy@gmail.com>
2022-06-27 07:09:56 +0100

    fix rendering errors

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -21,8 +21,8 @@ class Question extends Component {
         <div className='Question-status'>
           <img
             className='category'
-            alt={`${category.toLowerCase()}`}
-            src={`${category.toLowerCase()}.svg`}
+            alt={`${category.type.toLowerCase()}`}
+            src={`${category.type.toLowerCase()}.svg`}
           />
           <div className='difficulty'>Difficulty: {difficulty}</div>
           <img
